35b19fe5a6fca2ef30f834001631d2d369ad8d64
Yaoyu Chen <yaoyuchen@eml.cc>
2024-05-16 22:33:33 +0200

    Adjust `TopMenu` layout

--- src/lib/menus/TopMenu.svelte
+++ src/lib/menus/TopMenu.svelte
@@ -4,12 +4,6 @@
 
 <div class="pointer-events-none absolute left-0 top-0 z-10 flex w-full">
     <div class="m-2 flex w-full justify-between">
-        <div
-            class="pointer-events-auto flex rounded border bg-white/70 p-1 px-2 shadow-sm backdrop-blur-xl"
-        >
-            <span class="font-mono text-sm"> {nodesCount} nodes on canvas</span>
-        </div>
-
         <div class="flex space-x-2">
             <!-- Goofy animation applied because no one is looking yet -->
             <div
@@ -26,6 +20,25 @@
             >
                 <span class="inline-flex items-center font-mono text-sm"
                     >load setup</span
+                >
+            </div>
+
+            <div
+                class="pointer-events-auto flex border bg-white/70 p-1 px-2 backdrop-blur-xl"
+            >
+                <span class="inline-flex items-center font-mono text-sm">
+                    {nodesCount} nodes on canvas</span
+                >
+            </div>
+        </div>
+
+        <div class="flex space-x-2">
+            <!-- Goofy animation applied because no one is looking yet -->
+            <div
+                class="pointer-events-auto flex cursor-pointer rounded border bg-white/70 p-1 px-2 shadow-sm backdrop-blur-xl transition-all hover:-rotate-12"
+            >
+                <span class="inline-flex items-center font-mono text-sm"
+                    >library</span
                 >
             </div>
         </div>
