a40178a34562f25d03f41299213b7da22918453f
Yaoyu Chen <yaoyuchen@eml.cc>
2024-05-17 01:15:52 +0200

    Load nodeTypes dynamically

--- src/lib/menus/LibraryMenu.svelte
+++ src/lib/menus/LibraryMenu.svelte
@@ -13,28 +13,29 @@
 
 <div class="pointer-events-none absolute right-0 top-10 z-10 flex w-full">
     <div class="m-2 flex w-full justify-end">
-        <div class="flex space-x-2">
+        <div class="flex space-x-2 text-sm">
             <div
-                class="pointer-events-auto flex w-96 flex-col rounded border bg-white/70 shadow-sm backdrop-blur-xl"
+                class="pointer-events-auto flex w-48 flex-col rounded border bg-white/70 shadow-sm backdrop-blur-xl"
             >
                 <!-- Unstylized input -->
                 <input
                     type="text"
-                    class="my-1 w-full px-2 py-1 focus:outline-none"
+                    class="my-1 w-full px-4 py-2 focus:outline-none"
                     placeholder="Search..."
                 />
                 <!-- Divider -->
                 <div class="border-b"></div>
-                <div class="p-2">
-                    <div
-                        class="cursor-default rounded-sm p-2 transition-colors hover:bg-slate-100"
-                        on:dragstart={(event) =>
-                            onDragStart(event, 'ColumnSelector')}
-                        draggable={true}
-                    >
-                        Select Column
+                {#each Object.keys(nodeTypes) as type}
+                    <div class="px-2 pt-2 last:pb-2">
+                        <div
+                            class="cursor-default rounded-sm p-2 transition-colors hover:bg-slate-100"
+                            on:dragstart={(event) => onDragStart(event, type)}
+                            draggable={true}
+                        >
+                            {type}
+                        </div>
                     </div>
-                </div>
+                {/each}
             </div>
         </div>
     </div>
