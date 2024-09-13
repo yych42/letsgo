c6031ef3a981a2036ce1378413a23034b90a7cde
Yaoyu Chen <43839115+yych42@users.noreply.github.com>
2024-09-13 13:30:17 +0200

    move files

--- src/routes/flow/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { SvelteFlowProvider } from '@xyflow/svelte'
-    import '@xyflow/svelte/dist/style.css'
-    import Flow from '$lib/Flow.svelte'
-</script>
-
-<SvelteFlowProvider>
-    <Flow></Flow>
-</SvelteFlowProvider>
