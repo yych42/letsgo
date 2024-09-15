98bd9a4fb1e57e81567aa2a277acae0b21488f6c
Yaoyu Chen <43839115+yych42@users.noreply.github.com>
2024-09-15 20:10:06 +0200

    remove dev tools

--- src/lib/nodes/dev-tools/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Variable } from './Variable.svelte'
-export { default as Transform } from './Transform.svelte'
-export { default as Inspector } from './Inspector.svelte'
--- src/lib/nodes/index.ts
+++ src/lib/nodes/index.ts
@@ -1,9 +1,7 @@
 import type { NodeTypesExt } from '../types'
 
-import * as devTools from './dev-tools'
 import * as commons from './commons'
 
 export const nodeTypes: NodeTypesExt = {
-    ...devTools,
     ...commons
 }
